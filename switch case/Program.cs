ebb7d3fbb2d98b28bcf2e176e13c661d7faceb4b
akashkuchankar <kuchankarakash@gmail.com>
2022-10-03 09:42:38 -0700

    Add project files.

---
+++ switch case/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace switch_case
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter your language");
+            Console.WriteLine("1.marathi");
+            Console.WriteLine("2.hindhi");
+            Console.WriteLine("3.english");
+            int A = Convert.ToInt32(Console.ReadLine());
+            switch(A)
+            {
+                case 1: Console.WriteLine("you have selected marathi");
+                    break;
+                    case 2: Console.WriteLine("you have selected hindhi");
+                    break ;
+                    case 3: Console.WriteLine("you have selected english");
+                    break;
+                    default: Console.WriteLine("wrong number choose");
+                    break;
+
+            }
+        }
+    }
+}
